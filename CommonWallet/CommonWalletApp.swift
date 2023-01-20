4c40a728e79034d175a04780f03c5cbcb84d37e7
Kota <maedakoutaa@gmail.com>
2023-01-20 09:10:19 +0900

    Initial Commit

---
+++ CommonWallet/CommonWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonWalletApp.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct CommonWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
