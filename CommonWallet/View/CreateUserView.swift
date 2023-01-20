9624cd5f975a67fe063861e4c74786bea46ddf12
Kota <maedakoutaa@gmail.com>
2023-01-20 10:09:29 +0900

    add: LoginViewとCreateUserViewの作成

---
+++ CommonWallet/View/CreateUserView.swift
@@ -0,0 +1,54 @@
+//
+//  CreateUserView.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/20.
+//
+
+import SwiftUI
+
+struct CreateUserView: View {
+    @State var name = ""
+    @State var mailAdress = ""
+    @State var password = ""
+    @State private var isSecondView: Bool = false
+    @Binding var isShow: Bool
+    let authManager = AuthManager()
+
+    var body: some View {
+        VStack {
+            Text("アカウント登録")
+            TextField("名前", text: $name)
+            TextField("メールアドレス", text: $mailAdress)
+            TextField("パスワード", text: $password)
+
+            Button(action: {
+
+                authManager.createUser(email: mailAdress, password: password, name: name, complition: { isSuccess, message in
+                    isSecondView = isSuccess
+                    print("認証状況：", isSuccess, message)
+                })
+            }) {
+                Text("アカウント登録").fontWeight(.bold).font(.largeTitle)
+            }
+            .sheet(isPresented: self.$isSecondView) {
+                // trueになれば下からふわっと表示
+                ContentView()
+            }
+
+            Button {
+                isShow = false
+            } label: {
+                Text("ログインはこちら")
+            }
+        }
+        .padding()
+    }
+}
+
+//struct CreateUserView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CreateUserView()
+//    }
+//}
+
---
+++ CommonWallet/View/LoginView.swift
@@ -0,0 +1,55 @@
+//
+//  LoginView.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/20.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct LoginView: View {
+    @State var mailAdress = ""
+    @State var password = ""
+    @State private var isSecondView: Bool = false
+    @State private var isCreateUserView: Bool = false
+    let authManager = AuthManager()
+
+    var body: some View {
+        VStack {
+            Text("ログイン")
+            TextField("メールアドレス", text: $mailAdress)
+            TextField("パスワード", text: $password)
+
+            Button(action: {
+
+                authManager.login(email: mailAdress, password: password, complition: { isSuccess, message in
+                    isSecondView = isSuccess
+                    print("認証状況：", isSuccess, message)
+                })
+            }) {
+                Text("ログイン").fontWeight(.bold).font(.largeTitle)
+            }
+            .sheet(isPresented: self.$isSecondView) {
+                // trueになれば下からふわっと表示
+                ContentView()
+            }
+
+            Button {
+                isCreateUserView.toggle()
+            } label: {
+                Text("アカウント登録はこちら")
+            }
+            .sheet(isPresented: $isCreateUserView) {
+                CreateUserView(isShow: $isCreateUserView)
+            }
+        }
+        .padding()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
