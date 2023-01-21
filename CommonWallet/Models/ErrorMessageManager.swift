c435b7c1fcee9dd3da6653f65253dfc276744941
Kota <maedakoutaa@gmail.com>
2023-01-21 15:02:49 +0900

    fix: AuthManagerの関数をリファクタリング

---
+++ CommonWallet/Models/ErrorMessageManager.swift
@@ -0,0 +1,56 @@
+//
+//  FirebaseErrorManager.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/21.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+class ErrorMessageManager {
+
+    func getAuthErrorMessage(_ error:Error?) -> String {
+        if let error = error as NSError? {
+            if let errorCode = AuthErrorCode.Code(rawValue: error.code) {
+                switch errorCode {
+                case .invalidEmail:
+                    return "メールアドレスの形式が違います。"
+                case .emailAlreadyInUse:
+                    return "このメールアドレスはすでに使われています。"
+                case .weakPassword:
+                    return "パスワードが弱すぎます。"
+                case .userNotFound, .wrongPassword:
+                    return "メールアドレス、またはパスワードが間違っています"
+                case .userDisabled:
+                    return "このユーザーアカウントは無効化されています"
+                case .networkError:
+                    return "ネットワークエラーが発生しました。"
+                default:
+                    return "予期せぬエラーが発生しました。\nしばらく時間を置いてから再度お試しください。"
+                }
+            }
+        }
+        return "不明なエラー"
+    }
+
+    func getFirestoreErrorMessage(_ error:Error?) -> String {
+        if let error = error as NSError? {
+            if let errorCode = FirestoreErrorCode.Code(rawValue: error.code) {
+                switch errorCode {
+                case .alreadyExists:
+                    return "既に存在しています。"
+                case .cancelled:
+                    return "操作はキャンセルされました。"
+                case .dataLoss:
+                    return "データが足りません。"
+                default:
+                    return  "予期せぬエラーが発生しました。\nしばらく時間を置いてから再度お試しください。"
+                }
+            }
+        }
+        return "不明なエラー"
+    }
+
+}
--- CommonWallet/View/ContentView.swift
+++ CommonWallet/View/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let authManager = AuthManager()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+
+            Button(action: {
+                Task {
+                    await authManager.signOut(complition: { isSuccess, message in
+                        if isSuccess {
+                            print("サインアウト成功！", message)
+                        } else {
+                            print("サインアウト失敗", message)
+                        }
+                    })
+                }
+            }) {
+                Text("サインアウト")
+            }
+
+            Button(action: {
+                Task {
+                    await authManager.deleteUser(complition: { isSuccess, message in
+                        if isSuccess {
+                            print("アカウント削除成功！", message)
+                        } else {
+                            print("アカウント削除失敗", message)
+                        }
+                    })
+                }
+            }) {
+                Text("アカウント削除")
+            }
+
+            
         }
         .padding()
     }
