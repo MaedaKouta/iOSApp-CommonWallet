c4c4d54380292ea8f963528cdb37d38f62a5f643
Kota <maedakoutaa@gmail.com>
2023-01-23 15:49:12 +0900

    add: AccountViewwo

--- CommonWallet/View/AccountView.swift
+++ CommonWallet/View/AccountView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("アカウント")
+                .navigationBarTitleDisplayMode(.inline)
+
+        }
     }
 }
 
