b93a3bb76e7fa07dd0140ffa284fd76c78b765af
Kota <maedakoutaa@gmail.com>
2023-01-23 14:39:02 +0900

    add: CommonWalletViewの作成

---
+++ CommonWallet/View/CommonWalletView.swift
@@ -0,0 +1,20 @@
+//
+//  CommonWalletTabView.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct CommonWalletView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommonWalletTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommonWalletView()
+    }
+}
