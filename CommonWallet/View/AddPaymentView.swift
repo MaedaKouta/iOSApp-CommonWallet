c9a8b089dd3e2c903c06ebd5ee574be0f9755883
Kota <maedakoutaa@gmail.com>
2023-01-23 18:23:08 +0900

    add: AddPaymentViewの追加

---
+++ CommonWallet/View/AddPaymentView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPaymentView.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/23.
+//
+
+import SwiftUI
+
+struct AddPaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPaymentView()
+    }
+}
