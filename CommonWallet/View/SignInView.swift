55b92965e6c3145221f982f0b75fbf09e2bc5226
Kota <maedakoutaa@gmail.com>
2023-01-23 11:23:46 +0900

    fix: リファクタリング

--- CommonWallet/View/SignInView.swift
+++ CommonWallet/View/SignInView.swift
@@ -32,8 +32,7 @@ struct SignInView: View {
             }) {
                 Text("ログイン").fontWeight(.bold).font(.largeTitle)
             }
-            .sheet(isPresented: self.$isSecondView) {
-                // trueになれば下からふわっと表示
+            .fullScreenCover(isPresented: self.$isSecondView) {
                 ContentView()
             }
 
@@ -42,7 +41,7 @@ struct SignInView: View {
             } label: {
                 Text("アカウント登録はこちら")
             }
-            .sheet(isPresented: $isCreateUserView) {
+            .fullScreenCover(isPresented: $isCreateUserView) {
                 CreateUserView(isShow: $isCreateUserView)
             }
         }
