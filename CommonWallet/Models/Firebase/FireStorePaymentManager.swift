89587cc8d407bb7fdd80456224ea23011e844527
Kota <maedakoutaa@gmail.com>
2023-01-26 09:54:28 +0900

    add: FireStorePaymentManagerにfetchPaidPayments追加

--- CommonWallet/Models/Firebase/FireStorePaymentManager.swift
+++ CommonWallet/Models/Firebase/FireStorePaymentManager.swift
@@ -37,4 +37,92 @@ class FireStorePaymentManager {
         }
     }
 
+    func fetchPaidPayments() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+
+        // TODO: パートナーの絞り込みも追加しないといけない
+        db.collection("Payments")
+            .whereField("userUID", isEqualTo: uid)
+            .whereField("isFinished", isEqualTo: true)
+            .order(by: "createdAt", descending: true)
+            .addSnapshotListener { snapShots, error in
+                if let error = error {
+                    print("FirestoreからPaymentsの取得に失敗", error)
+                    return
+                }
+                print("FirestoreからPaymentsの取得に成功")
+                var payments = [Payment]()
+
+                snapShots?.documents.forEach({ snapShot in
+                    let data = snapShot.data()
+                    guard let userUid: String = data["userUid"],
+                          let title: String = data["title"],
+                          let cost: Int = data["cost"],
+                          let isMyPayment: Bool = data["isMyPayment"],
+                          let createdAt = data["createdAt"],
+                          let isFinished: Bool = data["isFinished"] else {
+                        print("データにnilが発見されてエラー")
+                        return
+                    }
+
+                    let payment = Payment(
+                        userUid: userUid,
+                        title: title,
+                        cost: cost,
+                        isMyPayment: isMyPayment,
+                        createdAt: createdAt,
+                        isFinished: isFinished
+                    )
+
+                    payments.append(payment)
+                })
+
+                completion(payments, nil)
+            }
+    }
+
+    func fetchNotPaidPayments() {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+
+        // TODO: パートナーの絞り込みも追加しないといけない
+        db.collection("Payments")
+            .whereField("userUID", isEqualTo: uid)
+            .whereField("isFinished", isEqualTo: false)
+            .order(by: "createdAt", descending: true)
+            .addSnapshotListener { snapShots, error in
+                if let error = error {
+                    print("FirestoreからPaymentsの取得に失敗", error)
+                    return
+                }
+                print("FirestoreからPaymentsの取得に成功")
+                var payments = [Payment]()
+
+                snapShots?.documents.forEach({ snapShot in
+                    let data = snapShot.data()
+                    guard let userUid: String = data["userUid"],
+                          let title: String = data["title"],
+                          let cost: Int = data["cost"],
+                          let isMyPayment: Bool = data["isMyPayment"],
+                          let createdAt = data["createdAt"],
+                          let isFinished: Bool = data["isFinished"] else {
+                        print("データにnilが発見されてエラー")
+                        return
+                    }
+
+                    let payment = Payment(
+                        userUid: userUid,
+                        title: title,
+                        cost: cost,
+                        isMyPayment: isMyPayment,
+                        createdAt: createdAt,
+                        isFinished: isFinished
+                    )
+
+                    payments.append(payment)
+                })
+
+                completion(payments, nil)
+            }
+    }
+
 }
