b43cac04d2214a5841697ebfab13208ec837dc52
Kota <maedakoutaa@gmail.com>
2023-01-30 16:27:57 +0900

    add: ShareNumberモデルの作成

---
+++ CommonWallet/Models/Firebase/ShareNumber.swift
@@ -0,0 +1,17 @@
+//
+//  SharedNumber.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/01/30.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestore
+
+class ShareNumber {
+
+    func createShareNumber(_ error:Error?) {
+    }
+
+}
