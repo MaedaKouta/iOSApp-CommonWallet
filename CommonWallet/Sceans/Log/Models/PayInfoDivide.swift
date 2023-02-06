c64857b1d554708ff196cc61713bb1dd650edad9
Kota <maedakoutaa@gmail.com>
2023-02-07 08:57:02 +0900

    add: PayInfoDivideモデルの作成

---
+++ CommonWallet/Sceans/Log/Models/PayInfoDivide.swift
@@ -0,0 +1,12 @@
+//
+//  PayInfoDivide.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/02/07.
+//
+
+import Foundation
+
+class PayInfoDivide {
+
+}
--- CommonWallet/Sceans/Log/ViewModels/AllLogViewModel.swift
+++ CommonWallet/Sceans/Log/ViewModels/AllLogViewModel.swift
@@ -6,10 +6,20 @@
 //
 
 import Foundation
+import Parchment
 
 class AllLogViewModel: ObservableObject {
 
     @Published var paidPayments = [PayInfo]()
+
+    @Published var pagingItems = [
+        PagingIndexItem(index: 0, title: "1月"),
+        PagingIndexItem(index: 1, title: "2月"),
+        PagingIndexItem(index: 2, title: "3月"),
+        PagingIndexItem(index: 3, title: "4月"),
+        PagingIndexItem(index: 4, title: "5月"),
+        PagingIndexItem(index: 5, title: "6月"),
+    ]
 
     private var fireStorePaymentManager = FireStorePayInfoManager()
     private var userDefaultsManager = UserDefaultsManager()
