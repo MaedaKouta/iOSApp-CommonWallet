6942f2ac51ca2a30ab6232d627614d6735ae50b3
Kota <maedakoutaa@gmail.com>
2023-02-06 09:16:28 +0900

    add: ヘッダー部分を別のViewni

---
+++ CommonWallet/CommonViews/HeaderAccountView.swift
@@ -0,0 +1,55 @@
+//
+//  AccountHeaderView.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/02/06.
+//
+
+import SwiftUI
+
+struct HeaderAccountView: View {
+
+    @State private var isAccountView = false
+
+    var body: some View {
+        HStack {
+            Text("12月22日 日曜日")
+                .foregroundColor(.gray)
+                .font(.system(size: 14))
+                .padding(16)
+            Spacer()
+        }.frame(height: 16, alignment: .topLeading)
+        HStack {
+            Text("こんばんは")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(16)
+            Spacer()
+            Button ( action: {
+                isAccountView = true
+            }) {
+                Image("SampleIcon")
+                    .resizable()
+                    .scaledToFill()
+                    .overlay(RoundedRectangle(cornerRadius: 75).stroke(Color.gray, lineWidth: 1))
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 36, height: 36, alignment: .center)
+                    .clipShape(Circle()) // 正円形に切り抜く
+                    .padding(.trailing, 16)
+            }
+            .sheet(isPresented: self.$isAccountView) {
+                // trueになれば下からふわっと表示
+                SettingView()
+            }
+            // 下の1行でListをアイコンボタンしかタップできなくしている
+            .buttonStyle(BorderlessButtonStyle())
+        }
+
+    }
+}
+
+struct HeaderAccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderAccountView()
+    }
+}
--- CommonWallet/Sceans/Home/View/CommonWalletView.swift
+++ CommonWallet/Sceans/Home/View/CommonWalletView.swift
@@ -24,39 +24,7 @@ struct CommonWalletView: View {
             List {
                 VStack {
                     // ヘッダー
-                    HStack {
-                        Text("12月22日 日曜日")
-                            .foregroundColor(.gray)
-                            .font(.system(size: 14))
-                            .padding(16)
-                        Spacer()
-                    }.frame(height: 16, alignment: .topLeading)
-                    HStack {
-                        Text("こんばんは")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .padding(16)
-                        Spacer()
-                        Button ( action: {
-                            isAccountView = true
-                        }) {
-                            Image("SampleIcon")
-                                .resizable()
-                                .scaledToFill()
-                                .overlay(RoundedRectangle(cornerRadius: 75).stroke(Color.gray, lineWidth: 1))
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 36, height: 36, alignment: .center)
-                                .clipShape(Circle()) // 正円形に切り抜く
-                                .padding(.trailing, 16)
-                        }
-                        .sheet(isPresented: self.$isAccountView) {
-                            // trueになれば下からふわっと表示
-                            SettingView()
-                        }
-                        // 下の1行でListをアイコンボタンしかタップできなくしている
-                        .buttonStyle(BorderlessButtonStyle())
-                    }
-
+                    HeaderAccountView()
 
                     // パートナーとの差額表示（四角いViewで柔らかい感じに）
                     ZStack {
