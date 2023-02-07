661d88623372d3bdcd8362b1ae2671ec3549a71d
Kota <maedakoutaa@gmail.com>
2023-02-07 10:54:28 +0900

    add: divideByMonthをモデルに追加

--- CommonWallet/Sceans/Log/Models/PayInfoDivide.swift
+++ CommonWallet/Sceans/Log/Models/PayInfoDivide.swift
@@ -16,19 +16,31 @@ class PayInfoDivide {
 
     // 下のように使用している月数ごとにPayInfoを２重配列で格納する
     // [[初月のPayInfo], [次の月のPayInfo] ... ]
-    func divideByMonth(monthCount: Int) {
+    func divideByMonth(monthCount: Int) -> [[PayInfo]] {
 
-        guard let createdAt = userDefaultsManager.getUser()?.createdAt else { return }
+        var payInfoByMonth: [[PayInfo]] = []
 
         fireStorePayInfoManager.fetchUnpaidPayInfo(completion: { payInfos, error in
 
+            guard let payInfos = payInfos else { return }
+
+            for i in 0 ..< monthCount {
+                for payInfo in payInfos {
+                    if self.isEqualMonth(returnFromNowMonth: monthCount-i, compareDate: payInfo.createdAt.dateValue()) {
+                        payInfoByMonth[i].append(payInfo)
+                    }
+                }
+            }
+
         })
-        
+
+        return payInfoByMonth
+
     }
 
     // 第一引数に、今月から何ヶ月戻った月と比較するかを取る
-    func isEqualMonth(returnFromNowMonth: Int, compareDate: Date) -> Bool {
-        var compareSourceDate = Calendar.current.date(byAdding: .month, value: -1*returnFromNowMonth, to: nowDate)
+    private func isEqualMonth(returnFromNowMonth: Int, compareDate: Date) -> Bool {
+        guard let compareSourceDate = Calendar.current.date(byAdding: .month, value: -1*returnFromNowMonth, to: nowDate) else { return false }
         return Calendar.current.isDate(compareSourceDate, equalTo: compareDate, toGranularity: .month)
     }
 
