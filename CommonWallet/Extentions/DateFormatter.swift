e87c5693e8396b22b4f8b1c38a5222fd9838b56f
Kota <maedakoutaa@gmail.com>
2023-02-07 09:34:18 +0900

    add: DateFormatterのExtentions追加

---
+++ CommonWallet/Extentions/DateFormatter.swift
@@ -0,0 +1,26 @@
+//
+//  DateFormatter.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/02/07.
+//
+
+import Foundation
+
+extension DateFormatter {
+
+    enum Template: String {
+        case date = "yMd"     // 2017/1/1
+        case time = "Hms"     // 12:39:22
+        case full = "yMdkHms" // 2017/1/1 12:39:22
+        case onlyHour = "k"   // 17時
+        case era = "GG"       // "西暦" (default) or "平成" (本体設定で和暦を指定している場合)
+        case weekDay = "EEEE" // 日曜日
+    }
+
+    func setTemplate(_ template: Template) {
+        // optionsは拡張用の引数だが使用されていないため常に0
+        dateFormat = DateFormatter.dateFormat(fromTemplate: template.rawValue, options: 0, locale: .current)
+    }
+
+}
--- CommonWallet/Sceans/Log/Models/PayInfoDivide.swift
+++ CommonWallet/Sceans/Log/Models/PayInfoDivide.swift
@@ -9,4 +9,27 @@ import Foundation
 
 class PayInfoDivide {
 
+    private let fireStorePayInfoManager = FireStorePayInfoManager()
+    private var userDefaultsManager = UserDefaultsManager()
+
+    //private let calendar = Calendar(identifier: .gregorian)
+    private let nowDate = Date()
+    private let dateFormat = DateFormatter.dateFormat(fromTemplate: .full, options: 0, locale: .current)
+
+    // 下のように使用している月数ごとにPayInfoを２重配列で格納する
+    // [[初月のPayInfo], [次の月のPayInfo] ... ]
+    func divideByMonth(monthCount: Int) {
+        
+        guard let createdAt = userDefaultsManager.getUser()?.createdAt else { return }
+
+        fireStorePayInfoManager.fetchUnpaidPayInfo(completion: { payInfos, error in
+
+        })
+        
+    }
+
+    func isEqualMonth(date1: Date, date2: Date) -> Bool {
+        return Calendar.current.isDate(date1, equalTo: date2, toGranularity: .month)
+    }
+
 }
