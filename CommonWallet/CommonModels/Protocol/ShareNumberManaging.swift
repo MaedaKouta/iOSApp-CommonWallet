f968b29abceb4af20e2e8ea294ead61dd2ef22b9
Kota <maedakoutaa@gmail.com>
2023-05-15 07:14:33 +0900

    fix: createShareNumberの修正

---
+++ CommonWallet/CommonModels/Protocol/ShareNumberManaging.swift
@@ -0,0 +1,12 @@
+//
+//  ShareNumberManaging.swift
+//  CommonWallet
+//
+//  Created by 前田航汰 on 2023/05/15.
+//
+
+import Foundation
+
+protocol ShareNumberManaging {
+    func createShareNumber() async -> String
+}
