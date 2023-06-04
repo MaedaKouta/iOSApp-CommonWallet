0853f7ee132a92a7a310a439f037e6806969216c
Kota <maedakoutaa@gmail.com>
2023-06-04 11:47:05 +0900

    add: UserdefaultsExtention作成

--- CommonWallet/Extentions/Userdefaults.swift
+++ CommonWallet/Extentions/Userdefaults.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension UserDefaults {
+
+    // 保存したいUIImage, 保存するUserDefaults, Keyを取得
+    func setUIImageToData(image: UIImage, forKey: String) {
+        let nsdata = image.pngData()
+        self.set(nsdata, forKey: forKey)
+    }
+
+    // 参照するUserDefaults, Keyを取得, UIImageを返す
+    func image(forKey: String) -> UIImage {
+        let data = self.data(forKey: forKey)
+        let returnImage = UIImage(data: data!)
+        return returnImage!
+    }
+
+}
