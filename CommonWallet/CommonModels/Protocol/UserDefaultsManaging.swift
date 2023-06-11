aeb00eddbdee39299ffc2af499ff7bb86d30fdc3
Kota <maedakoutaa@gmail.com>
2023-06-11 13:04:32 +0900

    UserDefaultsManaging.swift実装

--- CommonWallet/CommonModels/Protocol/UserDefaultsManaging.swift
+++ CommonWallet/CommonModels/Protocol/UserDefaultsManaging.swift
@@ -2,13 +2,45 @@
 //  UserDefaultsManaging.swift
 //  CommonWallet
 //
-//  Created by 前田航汰 on 2023/04/22.
-//
 
 import Foundation
 
-protocol UserDefaultsManagerProtocol {
+protocol UserDefaultsManaging {
+
+    var userDefaultsKey: UserDefaultsKey { get }
+
+    // MARK: - Setter
+    func setUser(user: User)
+    func setMyUserName(userName: String)
+    func setPartner(userId: String, name: String, iconPath: String, iconData: Data, shareNumber: String)
+    func setPartner(userId: String, name: String, shareNumber: String)
+    func setPartnerUserId(userId: String)
+    func setPartnerName(name: String)
+    func setPartnerShareNumber(shareNumber: String)
+    func setPartnerModifiedName(name: String)
+    func setOldestResolvedDate(date: Date)
+    func setMyIcon(path: String, imageData: Data)
+    func setPartnerIcon(path: String, imageData: Data)
+    func setIsSignedIn(isSignedIn: Bool)
+    func setLaunchedVersion(version: String)
+
+    // MARK: - Getter
     func getUser() -> User?
-    func getPartnerUid() -> String?
+    func getShareNumber() -> String?
+    func getPartnerUserId() -> String?
     func getPartnerName() -> String?
+    func getPartnerModifiedName() -> String?
+    func getPartnerShareNumber() -> String?
+    func getOldestResolvedDate() -> Date?
+    func getMyIconImageData() -> Data?
+    func getMyIconImagePath() -> String?
+    func getPartnerIconImageData() -> Data?
+    func getPartnerIconImagePath() -> String?
+    func getIsSignedIn() -> Bool?
+    func getLaunchedVersion() -> String?
+
+    // MARK: Delete
+    func clearUser()
+    func deletePartner()
+
 }
