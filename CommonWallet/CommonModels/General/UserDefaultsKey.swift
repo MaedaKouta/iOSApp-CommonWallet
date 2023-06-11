a0564f6bd42f598d4c65254922838aa8d4f340a0
Kota <maedakoutaa@gmail.com>
2023-06-11 13:04:10 +0900

    UserDefaultsKey.swiftにドキュメンテーションコメント追加

--- CommonWallet/CommonModels/General/UserDefaultsKey.swift
+++ CommonWallet/CommonModels/General/UserDefaultsKey.swift
@@ -5,29 +5,52 @@
 
 import Foundation
 
-/*
- UserDefaultsのKeyを保存するだけのModel
+/**
+ UserDefaultsのKey情報
+ - Description
+    - Myユーザー情報
+        - userId
+        - userName
+        - shareNumber
+        - email
+        - myIconPath
+        - myIconData
+        - createdAt
+        - oldestResolvedDate
+    - Partnerユーザー情報
+        - partnerUserId
+        - partnerName
+        - partnerIconPath
+        - partnerIconData
+        - partnerModifiedName
+        - partnerShareNumber
+    - サインイン情報
+        - isSignedIn
+    - バージョン情報
+        - launchedVersion
  */
 struct UserDefaultsKey {
 
-    // サインイン情報
-    let isSignedIn = "isSignedIn"
-
-    // ユーザー情報
+    // Myユーザー情報
     let userId = "userId"
     let userName = "userName"
     let shareNumber = "shareNumber"
     let email = "email"
     let myIconPath = "myIconPath"
     let myIconData = "myIconData"
-    let partnerIconPath = "partnerIconPath"
-    let partnerIconData = "partnerIconData"
     let createdAt = "createdAt"
+    let oldestResolvedDate = "oldestResolvedDate"
+
+    // Partnerユーザー情報
     let partnerUserId = "partnerUserId"
     let partnerName = "partnerName"
+    let partnerIconPath = "partnerIconPath"
+    let partnerIconData = "partnerIconData"
     let partnerModifiedName = "partnerModifiedName"
     let partnerShareNumber = "partnerShareNumber"
-    let oldestResolvedDate = "oldestResolvedDate"
+
+    // サインイン情報
+    let isSignedIn = "isSignedIn"
 
     // バージョン情報
     let launchedVersion = "launchedVersion"
