b793a92e3bf58b387639059483f2571f7813fc7d
Kota <maedakoutaa@gmail.com>
2023-06-11 13:04:59 +0900

    fix: UserDefaultsManagingプロトコルに準拠

--- CommonWallet/CommonModels/General/UserDefaultsManager.swift
+++ CommonWallet/CommonModels/General/UserDefaultsManager.swift
@@ -2,18 +2,16 @@
 //  UserDefaultsManager.swift
 //  CommonWallet
 //
-//  Created by 前田航汰 on 2023/01/22.
-//
 
 import Foundation
 import SwiftUI
 
-struct UserDefaultsManager {
+struct UserDefaultsManager: UserDefaultsManaging {
 
-    private let userDefaultsKey = UserDefaultsKey()
+    internal let userDefaultsKey = UserDefaultsKey()
 
     // MARK: - Setter
-    mutating func setUser(user: User) {
+    func setUser(user: User) {
         UserDefaults.standard.set(user.id, forKey: userDefaultsKey.userId)
         UserDefaults.standard.set(user.name, forKey: userDefaultsKey.userName)
         UserDefaults.standard.set(user.email, forKey: userDefaultsKey.email)
@@ -33,11 +31,11 @@ struct UserDefaultsManager {
         }
     }
 
-    mutating func setMyUserName(userName: String) {
+    func setMyUserName(userName: String) {
         UserDefaults.standard.set(userName, forKey: userDefaultsKey.userName)
     }
 
-    mutating func setPartner(userId: String, name: String, iconPath: String, iconData: Data, shareNumber: String) {
+    func setPartner(userId: String, name: String, iconPath: String, iconData: Data, shareNumber: String) {
         UserDefaults.standard.set(userId, forKey: userDefaultsKey.partnerUserId)
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerName)
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerModifiedName)
@@ -46,45 +44,53 @@ struct UserDefaultsManager {
         UserDefaults.standard.set(shareNumber, forKey: userDefaultsKey.partnerShareNumber)
     }
 
-    mutating func setPartner(userId: String, name: String, shareNumber: String) {
+    func setPartner(userId: String, name: String, shareNumber: String) {
         UserDefaults.standard.set(userId, forKey: userDefaultsKey.partnerUserId)
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerName)
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerModifiedName)
         UserDefaults.standard.set(shareNumber, forKey: userDefaultsKey.partnerShareNumber)
     }
 
-    mutating func setPartnerUserId(userId: String) {
+    func setPartnerUserId(userId: String) {
         UserDefaults.standard.set(userId, forKey: userDefaultsKey.partnerUserId)
     }
 
-    mutating func setPartnerName(name: String) {
+    func setPartnerName(name: String) {
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerName)
     }
 
-    mutating func setPartnerShareNumber(shareNumber: String) {
+    func setPartnerShareNumber(shareNumber: String) {
         UserDefaults.standard.set(shareNumber, forKey: userDefaultsKey.partnerShareNumber)
     }
 
-    mutating func setPartnerModifiedName(name: String) {
+    func setPartnerModifiedName(name: String) {
         UserDefaults.standard.set(name, forKey: userDefaultsKey.partnerModifiedName)
     }
 
-    mutating func setOldestResolvedDate(date: Date) {
+    func setOldestResolvedDate(date: Date) {
         UserDefaults.standard.set(date, forKey: userDefaultsKey.oldestResolvedDate)
     }
 
-    mutating func setMyIcon(path: String, imageData: Data) {
+    func setMyIcon(path: String, imageData: Data) {
         UserDefaults.standard.set(path, forKey: userDefaultsKey.myIconPath)
         UserDefaults.standard.set(imageData, forKey: userDefaultsKey.myIconData)
     }
 
-    mutating func setPartnerIcon(path: String, imageData: Data) {
+    func setPartnerIcon(path: String, imageData: Data) {
         UserDefaults.standard.set(path, forKey: userDefaultsKey.partnerIconPath)
         UserDefaults.standard.set(imageData, forKey: userDefaultsKey.partnerIconData)
     }
 
+    func setIsSignedIn(isSignedIn: Bool) {
+        UserDefaults.standard.set(isSignedIn, forKey: userDefaultsKey.isSignedIn)
+    }
+
+    func setLaunchedVersion(version: String) {
+        UserDefaults.standard.set(version, forKey: userDefaultsKey.launchedVersion)
+    }
+
     // MARK: - Getter
-    mutating func getUser() -> User? {
+    func getUser() -> User? {
         guard let userName = UserDefaults.standard.string(forKey: userDefaultsKey.userName),
               let email = UserDefaults.standard.string(forKey: userDefaultsKey.email),
               let userId = UserDefaults.standard.string(forKey: userDefaultsKey.userId),
@@ -102,48 +108,60 @@ struct UserDefaultsManager {
         return user
     }
 
-    mutating func getShareNumber() -> String? {
+    func getShareNumber() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.shareNumber)
     }
 
-    mutating func getPartnerUserId() -> String? {
+    func getShareNumber2() -> String? {
+        return UserDefaults.standard.string(forKey: userDefaultsKey.shareNumber)
+    }
+
+    func getPartnerUserId() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.partnerUserId)
     }
 
-    mutating func getPartnerName() -> String? {
+    func getPartnerName() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.partnerName)
     }
 
-    mutating func getPartnerModifiedName() -> String? {
+    func getPartnerModifiedName() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.partnerModifiedName)
     }
 
-    mutating func getPartnerShareNumber() -> String? {
+    func getPartnerShareNumber() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.partnerShareNumber)
     }
 
-    mutating func getOldestResolvedDate() -> Date? {
+    func getOldestResolvedDate() -> Date? {
         return UserDefaults.standard.object(forKey: userDefaultsKey.oldestResolvedDate) as? Date
     }
 
-    mutating func getMyIconImageData() -> Data? {
+    func getMyIconImageData() -> Data? {
         return UserDefaults.standard.data(forKey: userDefaultsKey.myIconData)
     }
 
-    mutating func getMyIconImagePath() -> String? {
+    func getMyIconImagePath() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.myIconPath)
     }
 
-    mutating func getPartnerIconImageData() -> Data? {
+    func getPartnerIconImageData() -> Data? {
         return UserDefaults.standard.data(forKey: userDefaultsKey.partnerIconData)
     }
 
-    mutating func getPartnerIconImagePath() -> String? {
+    func getPartnerIconImagePath() -> String? {
         return UserDefaults.standard.string(forKey: userDefaultsKey.partnerIconPath)
     }
 
+    func getIsSignedIn() -> Bool? {
+        return UserDefaults.standard.bool(forKey: userDefaultsKey.isSignedIn)
+    }
+
+    func getLaunchedVersion() -> String? {
+        return UserDefaults.standard.string(forKey: userDefaultsKey.launchedVersion)
+    }
+
     // MARK: Delete
-    mutating func clearUser() {
+    func clearUser() {
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.userId)
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.userName)
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.email)
@@ -152,28 +170,10 @@ struct UserDefaultsManager {
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.partnerName)
     }
 
-    mutating func deletePartner() {
+    func deletePartner() {
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.partnerUserId)
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.partnerName)
         UserDefaults.standard.set(nil, forKey: userDefaultsKey.partnerShareNumber)
-    }
-
-    var isSignedIn: Bool? {
-        get {
-            return UserDefaults.standard.bool(forKey: userDefaultsKey.isSignedIn)
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: userDefaultsKey.isSignedIn)
-        }
-    }
-
-    var launchedVersion: String? {
-        get {
-            return UserDefaults.standard.string(forKey: userDefaultsKey.launchedVersion)
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: userDefaultsKey.launchedVersion)
-        }
     }
 
 }
