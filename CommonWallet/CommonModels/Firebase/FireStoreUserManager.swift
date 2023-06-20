d4e6eb47512db75d27134f0ae9b836915f628a6b
Kota <maedakoutaa@gmail.com>
2023-06-21 07:29:10 +0900

    add: FireStoreUserManagerにドキュメントコメント追加

--- CommonWallet/CommonModels/Firebase/FireStoreUserManager.swift
+++ CommonWallet/CommonModels/Firebase/FireStoreUserManager.swift
@@ -2,13 +2,12 @@
 //  FireStoreManager.swift
 //  CommonWallet
 //
-//  Created by 前田航汰 on 2023/01/22.
-//
 
 import Foundation
 import FirebaseAuth
 import Firebase
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct FireStoreUserManager: FireStoreUserManaging {
 
@@ -16,8 +15,15 @@ struct FireStoreUserManager: FireStoreUserManaging {
     private var userDefaultsManager = UserDefaultsManager()
 
     // MARK: POST
+    /**
+     User情報を初期登録
+     - parameter userId: ユーザーID
+     - parameter userName: ユーザー名
+     - parameter email: メール
+     - parameter iconPath: アイコンのパス
+     - parameter shareNumber: 共有番号
+     */
     func createUser(userId: String, userName: String, email: String, iconPath: String, shareNumber: String) async throws {
-
         let user: Dictionary<String, Any> = ["id": userId,
                                              "name": userName,
                                              "email": email,
@@ -29,19 +35,48 @@ struct FireStoreUserManager: FireStoreUserManaging {
     }
 
     // MARK: PUT
+    /**
+     自分のUserNameを更新
+     - parameter userId: 自分のユーザーID
+     - parameter userName: 新しく登録するユーザー名
+     */
     func putUserName(userId: String, userName: String) async throws {
         let user: Dictionary<String, Any> = ["name": userName]
         try await db.collection("Users").document(userId).setData(user, merge: true)
     }
 
     // MARK: Delete
+    /**
+     自分のUser情報を削除
+     - parameter userId: 自分のユーザーID
+     */
     func deleteUser(userId: String) async throws {
         try await db.collection("Users").document(userId).delete()
     }
 
     // MARK: Fetch
-    func fetchInfo(userId: String, completion: @escaping(User?, Error?) -> Void) {
+    /**
+     Uesr情報を返す
+     - Description
+     - FireStoreのアクセス回数が増えるため使用注意
+     - parameter userId: 自分のユーザーID
+     - Returns: Userを返す
+     */
+    func fetchInfo(userId: String) async throws -> User? {
+        let user = try await db.collection("Users").document(userId)
+            .getDocument(as: User.self)
+        return user
+    }
 
+    /**
+     リアルタイムに自分のUser情報を取得する
+     - Description
+     - FirebaseのaddSnapshotListenerを利用
+     - 起動時に1度だけ呼び出しに利用
+     - parameter userId: 自分のユーザーID
+     - parameter completion: クロージャーでUserを返す
+     */
+    func realtimeFetchInfo(userId: String, completion: @escaping(User?, Error?) -> Void) {
         db.collection("Users").document(userId).addSnapshotListener { snapShot, error in
             if let error = error {
                 print("FireStore UserInfo Fetch Error")
