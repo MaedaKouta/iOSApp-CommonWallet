4f80e0bde1b5d7569df531e20ce2c3db9d2a9af7
Kota <maedakoutaa@gmail.com>
2023-06-21 07:22:48 +0900

    fix: TransactionのドメインをCodableに

--- CommonWallet/Domain/Transaction.swift
+++ CommonWallet/Domain/Transaction.swift
@@ -2,16 +2,14 @@
 //  PaymentData.swift
 //  CommonWallet
 //
-//  Created by 前田航汰 on 2023/01/23.
-//
 
 import Foundation
 import Firebase
-import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 struct Transaction: Codable, Identifiable {
-    public var id: String = UUID().uuidString
 
+    @DocumentID var id: String? = UUID().uuidString
     var creditorId: String? // 支払い者のID
     var debtorId: String? // 未払い者のID
     var title: String // タイトル
@@ -19,5 +17,16 @@ struct Transaction: Codable, Identifiable {
     var amount: Int // 金額
     var createdAt: Date // 作成日時
     var resolvedAt: Date? // 精算日時
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case creditorId
+        case debtorId
+        case title
+        case description
+        case amount
+        case createdAt
+        case resolvedAt
+    }
 
 }
