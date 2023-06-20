6f3dfab85a116c92f80d3ce35087edc583a3649e
Kota <maedakoutaa@gmail.com>
2023-06-21 07:23:17 +0900

    fix: UserのドメインをCodableに

--- CommonWallet/Domain/User.swift
+++ CommonWallet/Domain/User.swift
@@ -2,25 +2,37 @@
 //  User.swift
 //  CommonWallet
 //
-//  Created by 前田航汰 on 2023/01/22.
-//
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 struct User: Codable, Identifiable {
-    public var id: String = UUID().uuidString
 
+    @DocumentID var id : String? = UUID().uuidString
     var name: String
     var email: String
     var shareNumber: String // 共有番号
     var iconPath: String // アイコンのパス情報
     var createdAt: Date? // アカウント作成日時
-    var oldestResolvedDate: Date? // 精算の最も古い日時
-
-    // パートナー
     var partnerUserId: String?
     var partnerName: String?
+
+    // userDefaults専用
+    var oldestResolvedDate: Date? // 精算の最も古い日時
     var partnerShareNumber: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case email
+        case shareNumber
+        case iconPath
+        case createdAt
+        case oldestResolvedDate
+        case partnerUserId
+        case partnerName
+        case partnerShareNumber
+    }
 
 }
