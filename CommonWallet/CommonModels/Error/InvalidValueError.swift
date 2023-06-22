0e50e84f38a7c021fc805757224abc63f98cc359
Kota <maedakoutaa@gmail.com>
2023-06-22 15:24:44 +0900

    fix: InvalidValueErrorにunexpectedBlankTextエラー追加

--- CommonWallet/CommonModels/Error/InvalidValueError.swift
+++ CommonWallet/CommonModels/Error/InvalidValueError.swift
@@ -7,11 +7,14 @@ import Foundation
 
 enum InvalidValueError: Error {
     case unexpectedNullValue
+    case unexpectedBlankText
 
     var localizedDescription: String {
         switch self {
         case .unexpectedNullValue:
             return "unexpected null value"
+        case .unexpectedBlankText:
+            return "unexpected blank text"
         }
     }
 }
