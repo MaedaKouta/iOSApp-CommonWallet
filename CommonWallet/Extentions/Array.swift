9e7d76c2a50ca70d4bdc3cc53be13f5bfdeaa971
Kota <maedakoutaa@gmail.com>
2023-07-06 21:34:20 +0900

    add: ArrayExtention追加

--- CommonWallet/Extentions/Array.swift
+++ CommonWallet/Extentions/Array.swift
@@ -8,6 +8,14 @@ import Foundation
 extension Array {
 
     /**
+     インデックスが配列の範囲内の場合は配列の要素を返す
+     インデックスが配列の範囲外の場合はnilを返す
+    */
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+
+    /**
      配列を与えられた数ごとに分割し、二次元配列として返す
      - parameter chunkSize: 区切る数
      */
@@ -24,4 +32,5 @@ extension Array {
 
         return result
     }
+
 }
